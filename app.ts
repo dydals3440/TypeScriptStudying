094294ba0c22d1cfb098209398f455b4446fc203
김용민 <dydals3440@gmail.com>
2023-07-03 17:15:55 +0900

    feat: studying TS object

--- app.ts
+++ app.ts
@@ -1,16 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + n1 + n2);
-  } else {
-    return result;
-  }
-}
+const person: {
+  // 객체의 구조에 대한 정보를 제공하는 객체 타입 역할 (TS)
+  name: string;
+  age: number;
+} = {
+  name: 'Yongmin',
+  age: 30,
+};
 
-let number1: number = 5; // 5.0
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.name);
