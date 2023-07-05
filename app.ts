55486bfbf46d4984498478ecd2ea2d07eb9eff0e
김용민 <dydals3440@gmail.com>
2023-07-05 19:06:34 +0900

    feat: studying TS

--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
+// Union Type 다양한 타입 연산 하는법
 // 처음에는 number 타입으로 계산
-function add(n1: number | string, n2: number | string) {
+function add(
+  n1: number | string,
+  n2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof n1 === 'number' && typeof n2 === 'number') {
-    result = n1 + n2;
+  if (
+    (typeof n1 === 'number' && typeof n2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
   return result;
+  //   if (resultConversion === 'as-number') {
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
 // number타입 더하는 경우
-const addNumber = add(10, 20);
+const addNumber = add(10, 20, 'as-number');
 console.log(addNumber);
 
 // string타입을 더하고 싶은경우
-const addNames = add('matthew', 'Kim');
+const addNames = add('matthew', 'Kim', 'as-text');
 console.log(addNames);
 
 // string 타입으로 변경할 경우
