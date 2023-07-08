a1642751c02b228f790c3c8d3c47792e3974aaa2
김용민 <dydals3440@gmail.com>
2023-07-08 20:51:49 +0900

    feat: study unknown type

--- app.ts
+++ app.ts
@@ -1,48 +1,14 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+//  unknown type
+let userInput: unknown;
+
+userInput = 5;
+userInput = 'YONG';
+// unknown을 사용할 경우, useInput에 현재 저장된 타입을 확인해야 문자열을 원하는
+// 변수에 할당할 수 있습니다.
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-// void 타입: 의도적으로 반환문이 없다는 것을 의미
-// undefined도 타입으로 사용가능
-//  이 함수 자체는 아무것도 반환하지 않는다.
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-// 콜백 타입에 void가 반환이 되는데 그래도 결과값이 반환된다. 이것은 에러가 아니다.
-// 기본적으로 여기서 반환할 수 있는 모든 결과를 무시하게 되기 떄문입니다.
-function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
-  const result = n1 + n2;
-  callback(result);
-  // addAndHandle 내에서 어떤것도 return 하지 않겠다고 선언함.
-}
-// 콜백에 result 정의함
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-
-// printResult(add(5, 12));
-
-// let combineValues: (a: number, b: number) => number;
-
-// combineValues = add;
-
-// console.log(combineValues(8, 8)); // combineValues의 타입이 any기에 add함수가 할당되어 정상작동
-// combineValues = 5;
-// console.log(combineValues(8, 8)); // combineValues의 타입이 any기에 5가 할당되어 함수가 제대로 동작하지않음
-
-// // 함수라고 정의해주어 해결할 수 잇다.
-// let combineValues: Function;
-
-// // let someValue: undefined;
-
-// // // return 값 타입이 문자인 경우
-// // function repeat(text: string, count: number = 1): string {
-// //   let result: string = '';
-// //   while (count--) {
-// //     result += text;
-// //   }
-// //   return result;
-// // }
-
-// // console.log(repeat('ㅎㄷ', 2));
+// unknown / any 차이
+let userName: string;
+userName = userInput; // unknown은 string에 할당 X any타입으로 변경할시 에러해결
+// unknown은 any보다 제한적,
